5e318dbcb8138514c43d7da46db49425f21f3465
EliasRosendahl <jensen.eliasrosendahl@gmail.com>
2019-09-05 16:43:02 +0200

    adds server

---
+++ server.py
@@ -0,0 +1,23 @@
+
+import socket
+
+print("what")
+
+def main():
+    HOST = "127.0.0.1"
+    PORT = 9001
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    s.bind((HOST, PORT))
+    s.listen()
+    connection, address = s.accept()
+
+    with connection:
+        print("Connection from: ", address)
+        while True:
+            data = connection.recv(1024)
+            print(data)
+
+if __name__ == "__main__":
+    main()
